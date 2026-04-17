73aa7f93e9362bb652bace4a6bb59ae44f7daaa6
alenap <alenap@playtika.com>
2026-04-17 09:58:59 +0200

    selenium_homework_1

---
+++ src/test/java/webdriver/WebDriverTests.java
@@ -0,0 +1,25 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class WebDriverTests {
+
+    @Test
+    public void webDriverTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        List<WebElement> elements = driver.findElements(By.tagName("a"));
+        try {
+            elements.get(10).click();
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException(e.getMessage());
+        }
+        driver.quit();
+    }
+}
